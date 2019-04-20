2671006c585add12df9169a2736a5e69a8975fae
Randall Wilk <wilk.randall@gmail.com>
2019-04-20 09:53:21 -0500

    Update linkedin link

--- config/randallwilk.php
+++ config/randallwilk.php
@@ -7,7 +7,7 @@ return [
     'contact_email'            => 'randall@randallwilk.com',
     'contact_phone'            => '(715) 551 - 8945',
     'facebook_link'            => 'https://www.facebook.com/randall.j.wilk',
-    'linkedin_link'            => 'https://www.linkedin.com/in/randall-wilk-132b9968',
+    'linkedin_link'            => 'https://www.linkedin.com/in/randall-wilk',
     'github_link'              => 'https://github.com/rawilk',
     'project_placeholder'      => '/images/project-placeholder.png',
     'default_page_description' => 'I’m a full-stack web developer that specializes in Laravel and Vue. Web development is not only my career path, but also my passion and hobby.',
