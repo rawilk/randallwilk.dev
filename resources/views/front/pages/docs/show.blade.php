ffca9049165c136fe798656f54102ce6d5b97da7
Randall Wilk <22842525+rawilk@users.noreply.github.com>
2021-01-28 13:51:20 -0600

    Remove old code

--- resources/views/front/pages/docs/show.blade.php
+++ resources/views/front/pages/docs/show.blade.php
@@ -86,33 +86,4 @@
             </section>
         </div>
     </div>
-
-    <section class="wrap grid pb-24 gap-8 md:grid-cols-3 items-stretch hidden">
-        <div class="z-10 print:hidden">
-            @include('front.pages.docs.partials.navigation')
-        </div>
-
-        <div class="md:col-span-2">
-            @if ($showBigTitle)
-                <div class="mb-16">
-                    <h1 class="banner-slogan">{{ $repository->slug }}</h1>
-                    <div class="banner-intro flex items-center justify-start">
-                        {{ $alias->slogan }}
-                    </div>
-                </div>
-
-                <h2 class="title-xl mb-8">{{ $page->title }}</h2>
-            @else
-                <h1 class="title-xl mb-8">{{ $page->title }}</h1>
-            @endif
-
-            <div class="markup markup-titles markup-lists markup-code links-black links-underline no-title-uppercase {{ str_replace('/', '-', $page->slug) }}">
-                {!! $page->contents !!}
-            </div>
-
-            <div class="pt-8 pb-6 mt-12 border-t-2">
-                <x-doc-footer-links :previous="$alias->previousPage()" :next="$alias->nextPage()" />
-            </div>
-        </div>
-    </section>
 </x-page>
