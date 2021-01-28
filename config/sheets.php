a8448b76a9e8694c898e40ea9e1428c6c112cfbb
Randall Wilk <22842525+rawilk@users.noreply.github.com>
2021-01-28 09:03:16 -0600

    Remove commented out code

--- config/sheets.php
+++ config/sheets.php
@@ -10,7 +10,6 @@ return [
             'sheet_class' => \App\Docs\DocumentationPage::class,
             'path_parser' => \App\Docs\DocumentationPathParser::class,
             'content_parser' => \App\Docs\DocumentationContentParser::class,
-            // 'content_parser' => \App\Support\Sheets\ContentParser::class,
         ],
 
     ],
