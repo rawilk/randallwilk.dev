95e169a2874cddba3c8235061ba621c2aef9814d
Randall Wilk <wilk.randall@gmail.com>
2021-04-02 07:52:33 -0500

    Remove duplicate meta tags

--- resources/views/layouts/partials/meta.blade.php
+++ resources/views/layouts/partials/meta.blade.php
@@ -1,6 +1,3 @@
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
 <meta name="description" content="{{ $description ?? '' }}">
 <meta name="og:title" content="{{ $ogTitle ?? $title ?? '' }}">
 <meta name="og:description" content="{{ $ogDescription ?? $description ?? '' }}">
