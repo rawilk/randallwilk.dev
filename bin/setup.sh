7662ac3f1ba37e15b43b7b450a1b165db6b40d72
Randall Wilk <wilk.randall@gmail.com>
2022-07-22 08:41:31 -0500

    wip

--- bin/setup.sh
+++ bin/setup.sh
@@ -11,18 +11,11 @@ php artisan key:generate
 
 # Create database if not exists...
 if ! mysql -u root -e "use randallwilk"; then
-    # Database does not exist, create it now...
+    echo "Creating randallwilk database"
     mysql -u root -e "create database randallwilk"
 fi
 
 php artisan migrate
 
-# Pull in repo info and docs
-php artisan import:github-repositories
-php artisan import:github-issues
-php artisan import:packagist-downloads
-php artisan import:npm-downloads
-source "${dirname "$0"}/checkout_latest_docs.sh"
-
 npm install
-npx mix
+npm run build
