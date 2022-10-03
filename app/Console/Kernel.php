2a2f2f46435b288b489ce0ff8bafa27d30aef492
Randall Wilk <wilk.randall@gmail.com>
2022-10-03 14:32:38 -0500

    Schedule docs to update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\GenerateSitemapCommand;
+use App\Console\Commands\GitHub\ImportDocsFromRepositoriesCommand;
 use App\Console\Commands\GitHub\ImportGitHubRepositoriesCommand;
 use App\Console\Commands\GitHub\ImportPackagistDownloadsCommand;
 use App\Console\Commands\Npm\ImportNpmDownloadsCommand;
@@ -17,6 +18,7 @@ final class Kernel extends ConsoleKernel
         $schedule->command(ImportPackagistDownloadsCommand::class)->hourly();
         $schedule->command(ImportNpmDownloadsCommand::class)->weekly();
         $schedule->command(GenerateSitemapCommand::class)->daily();
+        $schedule->command(ImportDocsFromRepositoriesCommand::class)->everyThirtyMinutes()->runInBackground();
 
         // Model pruning...
         $schedule->command('queue:prune-batches --hours=48 --unfinished=72')->daily()->runInBackground();
