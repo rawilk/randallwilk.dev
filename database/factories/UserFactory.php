4e37cc8911bb6205e02974f37dc565d410cf900a
Randall Wilk <wilk.randall@gmail.com>
2022-10-04 09:39:34 -0500

    wip

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\User\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Rawilk\LaravelBase\Models\Role;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User\User>
@@ -12,6 +13,19 @@ use Illuminate\Support\Str;
 final class UserFactory extends Factory
 {
     protected $model = User::class;
+
+    private static ?array $roles = null;
+
+    public function configure(): self
+    {
+        return $this->afterCreating(function (User $user) {
+            if (is_array(self::$roles)) {
+                $user->assignRole(self::$roles);
+
+                self::$roles = null;
+            }
+        });
+    }
 
     /**
      * @return array<string, mixed>
@@ -26,5 +40,15 @@ final class UserFactory extends Factory
             'password' => 'secret',
             'remember_token' => Str::random(10),
         ];
+    }
+
+    public function superAdmin(): self
+    {
+        $roleModel = app(config('permission.models.role'));
+        $role = $roleModel::firstOrCreate(['name' => Role::$superAdminName], []);
+
+        self::$roles = [$role];
+
+        return $this;
     }
 }
