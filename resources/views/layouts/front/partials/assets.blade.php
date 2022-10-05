c3d789a63793fa488f9d534944d105e0d47327c9
Randall Wilk <wilk.randall@gmail.com>
2022-10-05 14:25:40 -0500

    wip

--- resources/views/layouts/front/partials/assets.blade.php
+++ resources/views/layouts/front/partials/assets.blade.php
@@ -1,6 +1,5 @@
 @include('layouts.partials.favicons')
 
 <link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;500;600;700&family=Noto+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,900;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
-{{--<link href="https://fonts.googleapis.com/css2?family=Fira+Mono:wght@400;500;700&display=swap" rel="stylesheet">--}}
 
 @vite(['resources/css/front.css', 'resources/js/app.js'])
