31b1398d42028564d54b04e31e081818bb486b12
Randall Wilk <wilk.randall@gmail.com>
2022-10-21 08:07:22 -0500

    Add x-cloak to the actual menu part

--- app/Services/Menus/Macros/FlyoutMenu.php
+++ app/Services/Menus/Macros/FlyoutMenu.php
@@ -21,6 +21,7 @@ final class FlyoutMenu implements MenuMacro
             ->setWrapperTag('div')
             ->addClass('absolute z-10 -ml-4 mt-3 w-screen max-w-md transform lg:max-w-2xl lg:left-1/2 lg:ml-0 lg:-translate-x-1/2')
             ->setAttribute('x-show', 'open')
+            ->setAttribute('x-cloak', '')
             ->setAttribute('x-transition:enter', 'transition ease-out duration-200')
             ->setAttribute('x-transition:enter-start', 'opacity-0 translate-y-1')
             ->setAttribute('x-transition:enter-end', 'opacity-100 translate-y-0')
