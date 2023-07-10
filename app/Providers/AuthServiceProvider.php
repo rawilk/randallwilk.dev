ee49fa9c245c329c7f0b9acc49b04d34fbf6e653
Randall Wilk <wilk.randall@gmail.com>
2023-07-10 08:38:10 -0500

    Remove registerPolicies method call from AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -11,15 +11,13 @@ final class AuthServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->registerPolicies();
-
         Gate::define('viewAdminPanel', function ($user) {
             // For now, only my super admin account can do this.
             return $user?->isSuperAdmin() ?? false;
         });
     }
 
-    public function register()
+    public function register(): void
     {
         parent::register();
 
