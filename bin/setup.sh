7ce76b9dd5df3071d0db97ff8a68f6e252437b38
Randall Wilk <wilk.randall@gmail.com>
2024-10-30 13:19:14 -0500

    Refresh install script

--- bin/setup.sh
+++ bin/setup.sh
@@ -9,41 +9,20 @@ composer install
 cp .env.example .env
 php artisan key:generate
 
-read -p "Decrypt .env.local? [y/n] " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    read -p "Enter encryption key: " ENCRYPTION_KEY
-    read -p "Overwrite .env file? [y/n] " -n 1 -r
-    echo
+# Database name
+DB_NAME="randallwilk"
 
-    php artisan env:decrypt --key=$ENCRYPTION_KEY --env=local --force
-    echo "Decrypted to .env.local"
-
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
-        php artisan env:decrypt --key=$ENCRYPTION_KEY --env=local --filename=.env --force
-        echo "Overwrite decrypted environment to .env"
-    fi
-fi
-
-# Create database if not exists...
-if ! mysql -u root -e "use randallwilk"; then
-    echo "Creating randallwilk database"
-    mysql -u root -e "create database randallwilk"
-fi
-
-# Create test database if not exists...
-if ! mysql -u root -e "use randallwilk_test"; then
-    echo "Creating randallwilk_test database"
-    mysql -u root -e "create database randallwilk_test"
+# Create the local db if it does not exist
+if ! psql -U postgres -lqt | cut -d \| -f 1 | grep -qw $DB_NAME; then
+    echo "Creating '$DB_NAME' database"
+    psql -U postgres -c "CREATE DATABASE $DB_NAME"
 fi
 
 php artisan migrate
 php artisan db:seed
 php artisan storage:link
-php artisan ide-helper:meta
-php artisan ide-helper:generate
 
 npm install
 npm run build
+
+echo "Install complete. Be sure to fill out GitHub credentials so docs can be imported."
