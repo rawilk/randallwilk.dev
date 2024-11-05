2115becc9d6db07c232cfd09e6df633e07fe6820
Randall Wilk <randall@randallwilk.dev>
2024-11-05 11:07:27 -0600

    Make font sheets async

--- resources/views/layouts/docs/partials/assets.blade.php
+++ resources/views/layouts/docs/partials/assets.blade.php
@@ -1,6 +1,6 @@
 @include('layouts.partials.favicons')
 
-<link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-<link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&family=Lexend:wght@100..900&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://rsms.me/inter/inter.css" async>
+<link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&family=Lexend:wght@100..900&display=swap" rel="stylesheet" async>
 
 @vite(['resources/css/front/docs/app.css', 'resources/js/docs.js'])
