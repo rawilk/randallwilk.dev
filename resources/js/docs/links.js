04f5ce1888778771cc2d21a7983499cee67f4ce9
Randall Wilk <randall@randallwilk.dev>
2024-11-05 08:52:19 -0600

    Remove wire:navigate from doc links

--- resources/js/docs/links.js
+++ resources/js/docs/links.js
@@ -3,15 +3,7 @@ const configureLinks = () => {
         const url = new URL(el.href);
         if (url.origin !== location.origin) {
             setLinkTargetToBlank(el);
-
-            return;
         }
-
-        if (! el.getAttribute('href').startsWith('/docs/')) {
-            return;
-        }
-
-        el.setAttribute('wire:navigate', '');
     });
 };
 
