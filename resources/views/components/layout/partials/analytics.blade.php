ef9a055b85eb657544fa09cf8b36e451fda207e3
Randall Wilk <randall@randallwilk.dev>
2024-11-05 10:42:29 -0600

    Update analytics script

--- resources/views/components/layout/partials/analytics.blade.php
+++ resources/views/components/layout/partials/analytics.blade.php
@@ -3,16 +3,13 @@
 ])
 
 @env('production')
-    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id={{ $id }}"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-
+        function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
+
         gtag('config', '{{ $id }}');
     </script>
 @endenv
