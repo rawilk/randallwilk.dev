6703fa7204ad356446dd0e3107803279d5bf3f00
Randall Wilk <22842525+rawilk@users.noreply.github.com>
2025-05-20 15:22:44 -0500

    Update user factory fields

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,6 +26,7 @@ class UserFactory extends Factory
             'remember_token' => Str::random(10),
             'is_admin' => false,
             'avatar_path' => null,
+            'github_username' => null,
         ];
     }
 
